266841fd3951a61c5d8d91b25a33a0040984327a
Sergey Aksenov <aksenovdev@gmail.com>
2017-12-14 22:32:46 +0700

    Create config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path'),
+  webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'main': './source/main.ts'
+  },
+  output: {
+    path: __dirname + '/build/',
+    filename: './[name].js'
+  },
+
+  resolve: {
+    extensions: ['', '.ts', '.tsx', '.js', '.jsx']
+  },
+
+  module: {
+    loaders: [      
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ],
+    rules: [
+      {
+          test: /\.ts$/,
+          enforce: 'pre',
+          loader: 'tslint-loader',
+          options: { }
+      }
+    ]
+  },
+  /*plugins:[
+    
+  ]*/
+};
